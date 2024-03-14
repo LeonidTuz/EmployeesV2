79696fa2bc489d6e9f6172973e1bd6d61ad46c14
leonidtuz <l.tuz@dodobrands.io>
2024-03-14 19:16:46 +0300

    add migrations

--- EmployeesV2.Api/Program.cs
+++ EmployeesV2.Api/Program.cs
@@ -22,6 +22,21 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Произошла ошибка при миграции базы данных.");
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
